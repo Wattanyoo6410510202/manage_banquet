69909a8c50bce62efb353e128127f8750cb56cef
Boat <creeda725@gmail.com>
2026-03-10 09:36:46 +0700

    Present 10/03

--- header.php
+++ header.php
@@ -51,6 +51,7 @@ function is_active($pages)
         <meta charset="UTF-8">
         <title>Sale System - Banquet</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="assets/img/images (1).png">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
         <link
